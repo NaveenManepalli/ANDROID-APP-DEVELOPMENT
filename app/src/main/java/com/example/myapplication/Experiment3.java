35fafbf4fa34888e4c1d125ddfbca4401bb8a63c
Naveen.B <86225990+NaveenManepalli@users.noreply.github.com>
2024-05-15 21:58:28 +0530

    expirement3 completed

--- app/src/main/java/com/example/myapplication/Experiment3.java
+++ app/src/main/java/com/example/myapplication/Experiment3.java
@@ -7,18 +7,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Experiment3 extends AppCompatActivity {
+    private EditText editTextAmount;
+    private RadioGroup radioGroupType;
+    private TextView textViewResult;
+    private double weeklyExpense = 0.0;
+    private double weeklyIncome = 0.0;
+
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_experiment3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        editTextAmount = findViewById(R.id.editTextAmount);
+        radioGroupType = findViewById(R.id.radioGroupType);
+        textViewResult = findViewById(R.id.textViewResult);
+    }
+    public void onAddButtonClick(View view) {
+        double amount;
+        try {
+            amount = Double.parseDouble(editTextAmount.getText().toString());
+        } catch (NumberFormatException e) {
+            amount = 0.0;
+        }
+
+        RadioButton radioButton =
+                findViewById(radioGroupType.getCheckedRadioButtonId());
+        String type = radioButton.getText().toString();
+        if (type.equals("Expense")) {
+            weeklyExpense += amount;
+        } else {
+            weeklyIncome += amount;
+        }
+        updateResultText();
+    }
+
+    private void updateResultText() {
+        String resultText = "Weekly Expense: $" + weeklyExpense + "\nWeekly Income: $"
+                + weeklyIncome;
+        textViewResult.setText(resultText);
     }
 }
